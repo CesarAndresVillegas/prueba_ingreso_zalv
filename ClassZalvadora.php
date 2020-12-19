f9c2945ac6992862c89157b9e41be03179134391
Cesar.Villegas.T <cesar@vendamasfacil.com>
2020-12-18 22:21:30 -0500

    method question 2 tested and added

--- ClassZalvadora.php
+++ ClassZalvadora.php
@@ -2,8 +2,12 @@
 
 class Zalvadora {
 
-    public static function question_2_1() {
-        return 1;
+    public static function question2_1($getParams) {
+        $multiplier = $getParams["N"];
+        $word = $getParams["S"];
+        $response = $multiplier && $word && is_numeric($multiplier) ?
+        str_repeat($word, $multiplier) :
+        'Los parámetros están incompletos o con formato errado. Recuerda que los parámetros requeridos son N (númerico - multiplica la palabra) y S (palabra que se multiplicada)';
+        return $response;
     }
-
 }
--- test/ClassZalvadoraTest.php
+++ test/ClassZalvadoraTest.php
@@ -6,9 +6,31 @@ require_once 'ClassZalvadora.php';
 use PHPUnit\Framework\TestCase;
 
 class ClassZalvadoraTest extends TestCase {
-    public function test_question2_1()
+    /**
+     * @test
+     * @dataProvider question2_1Provider
+     */
+    public function test_question2_1($multiplier, $word, $expected)
     {
-        $response = Zalvadora::question_2_1();
-        $this->assertEquals(1, $response);
+        $response = Zalvadora::question2_1(array('N' => $multiplier, 'S' => $word));
+        $this->assertEquals($expected, $response);
     }    
+
+    public function question2_1Provider() {
+        $errorText = 'Los parámetros están incompletos o con formato errado. Recuerda que los parámetros requeridos son N (númerico - multiplica la palabra) y S (palabra que se multiplicada)';
+        // primer elemento input N requerido en el problema
+        // segundo elemento input S requerido en el problema
+        // tercer elemento retorno esperado 
+        return [
+            'Prueba 1' => [2, 'Hola', 'HolaHola'],
+            'Prueba 2' => [3, '=)', '=)=)=)'],
+            'Prueba 3' => [0, 'Hola', $errorText],
+            'Prueba 4' => ['2', 'Hola', 'HolaHola'],
+            'Prueba 5' => [null, 'Hola', $errorText],
+            'Prueba 6' => [2, null, $errorText],
+            'Prueba 7' => [null, null, $errorText],
+            'Prueba 8' => ['hola', "Hola", $errorText],
+            'Prueba 9' => [3, 4, 444]
+        ];
+    }
 }
