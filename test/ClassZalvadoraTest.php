e2f706cc045dce8579babe41964c8e1e12e0e4fd
Cesar.Villegas.T <cesar@vendamasfacil.com>
2020-12-18 22:05:33 -0500

    unit test class added

---
+++ test/ClassZalvadoraTest.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'vendor/autoload.php';
+require_once 'ClassZalvadora.php';
+
+use PHPUnit\Framework\TestCase;
+
+class ClassZalvadoraTest extends TestCase {
+    public function test_question2_1()
+    {
+        $this->assertEquals(1,1);
+    }    
+}
