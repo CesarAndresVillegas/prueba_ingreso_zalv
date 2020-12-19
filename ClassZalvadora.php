3dc9d011b33b10c9dec656bddfe3b8dba98ae041
Cesar.Villegas.T <cesar@vendamasfacil.com>
2020-12-18 22:30:16 -0500

    prueba 2.1 ok

--- ClassZalvadora.php
+++ ClassZalvadora.php
@@ -1,10 +1,10 @@
 <?php
 
 class Zalvadora {
-
     public static function question2_1($getParams) {
         $multiplier = $getParams["N"];
         $word = $getParams["S"];
+        // verify N and S param and the types of var
         $response = $multiplier && $word && is_numeric($multiplier) ?
         str_repeat($word, $multiplier) :
         'Los parámetros están incompletos o con formato errado. Recuerda que los parámetros requeridos son N (númerico - multiplica la palabra) y S (palabra que se multiplicada)';
--- prueba2.1.php
+++ prueba2.1.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'ClassZalvadora.php';
 
-$response = "prueba recurso 2.1";
+$response = Zalvadora::question2_1($_GET);
 print_r($response);
